dc17a643f060605490e55a8a3841284d70580b1b
mango_gills <juzzgil21@gmail.com>
2022-05-05 12:13:45 +0800

    feat: scores, styling adjustments
    added scoring for user and cpu.
    set game for 5 rounds.
    player with higher score wins the game
    minor styling adjustments

--- script/script.js
+++ script/script.js
@@ -8,18 +8,40 @@ const gameStatus = document.getElementById("status");
 const userPick = document.getElementById("user-pick");
 const cpuPick = document.getElementById("cpu-pick");
 
+const userScore = document.getElementById("userScore");
+const cpuScore = document.getElementById("cpuScore");
+
+let userScoreCounter = 0;
+let cpuScoreCounter = 0;
+
+let gameCounter = 5;
+
 rpsButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    const rand = choiceRandomizer();
-    gamePlay(btn, rand);
+    if (gameCounter > 0) {
+      const rand = choiceRandomizer();
+      gamePlay(btn, rand);
+      cpuPick.innerText = rand.toUpperCase();
+      userPick.innerText = btn.classList[1].toUpperCase();
 
-    cpuPick.innerText = rand.toUpperCase();
-    userPick.innerText = btn.classList[1].toUpperCase();
-    gameStatus.style.transform = `rotate(2deg)`;
+      gameStatus.style.transform = `rotate(2deg)`;
 
-    setTimeout(() => {
-      gameStatus.style.transform = `rotate(0deg)`;
-    }, 400);
+      userScore.innerText = userScoreCounter;
+      cpuScore.innerText = cpuScoreCounter;
+
+      setTimeout(() => {
+        gameStatus.style.transform = `rotate(0deg)`;
+      }, 400);
+    } else {
+      if (parseInt(userScore.innerText) > parseInt(cpuScore.innerText)) {
+        alert("Game Over, You Won!");
+      } else if (parseInt(userScore.innerText) < parseInt(cpuScore.innerText)) {
+        alert("Game Over, CPU Won!");
+      } else {
+        alert(`Game Over, It's a draw!`);
+      }
+    }
+    gameCounter -= 1;
   });
 });
 
@@ -31,6 +53,11 @@ function clearStatus() {
   userPick.innerText = "";
   cpuPick.innerText = "";
   gameStatus.innerText = "";
+  userScore.innerText = 0;
+  cpuScore.innerText = 0;
+  userScoreCounter = 0;
+  cpuScoreCounter = 0;
+  gameCounter = 5;
 }
 
 function choiceRandomizer() {
@@ -52,8 +79,10 @@ function gamePlay(btn, random) {
     case btn.classList.contains("scissor"):
       if (random == "paper") {
         gameStatus.innerText = "You win";
+        userScoreCounter += 1;
       } else if (random == "rock") {
         gameStatus.innerText = "You lost";
+        cpuScoreCounter += 1;
       } else {
         gameStatus.innerText = "Draw";
       }
@@ -61,8 +90,10 @@ function gamePlay(btn, random) {
     case btn.classList.contains("paper"):
       if (random == "rock") {
         gameStatus.innerText = "You win";
+        userScoreCounter += 1;
       } else if (random == "scissor") {
         gameStatus.innerText = "You lost";
+        cpuScoreCounter += 1;
       } else {
         gameStatus.innerText = "Draw";
       }
@@ -70,8 +101,10 @@ function gamePlay(btn, random) {
     case btn.classList.contains("rock"):
       if (random == "scissor") {
         gameStatus.innerText = "You win";
+        userScoreCounter += 1;
       } else if (random == "paper") {
         gameStatus.innerText = "You lost";
+        cpuScoreCounter += 1;
       } else {
         gameStatus.innerText = "Draw";
       }
@@ -80,7 +113,7 @@ function gamePlay(btn, random) {
     default:
       break;
   }
-  return gameStatus;
+  return gameStatus, userScoreCounter, cpuScoreCounter;
 }
 
 resetBtn.addEventListener("click", () => {
